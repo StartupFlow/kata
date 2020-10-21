509cce00ef728e058e703b496c79cd0b3b4e9ffa
Javier Venegas <jota.montecino@gmail.com>
2020-10-21 11:52:10 +0200

    ADD: basic fizzbuzz signature to implement

---
+++ fizzbuzz/js/src/app.js
@@ -0,0 +1,13 @@
+// Impement fizzbuzz logic
+function fizzbuzz(number) {
+  return '0';
+}
+
+// Implement the wrapper so we can count from 0 to STOP.
+function main() {
+   for(let i=0; i<10;i++) {
+     console.log(fizzbuzz(i));
+   }
+}
+
+main();
